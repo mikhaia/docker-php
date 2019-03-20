3db242187ced8caf28c8e178e3661f241dc7fc62
Alexander Mikhailov <alexander@enclave.vn>
2019-03-20 13:10:52 +0700

    Add work example with connection php to mysql

--- web/public/index.php
+++ web/public/index.php
@@ -1,31 +1,15 @@
 <?php
-// var_dump($_SERVER['DOCUMENT_ROOT']);
-echo '<pre>';
-$d = dir('./../');
-
-echo "Handle: " . $d->handle . "<br>";
-echo "Path: " . $d->path . "<br>";
-
-while (($file = $d->read()) !== false){ 
-  echo "filename: " . $file . "<br>"; 
-} 
-$d->close(); 
-echo '</pre>';
-
-$con = mysqli_connect("db","root","root","test", 3306);
-
-$db = new mysqli("db","root","root","test", 3306);
-var_dump($db);
+$con = mysqli_connect("db","root","root","database", 3306);
 
 // Check connection
 if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-  else
-  {
-  	echo "ok";
-  }
+{
+	echo '<h3 style="color:red;text-align:center">Failed to connect to MySQL: ' . mysqli_connect_error().'</h3>';
+}
+else
+{
+	echo '<h3 style="color:green;text-align:center">The connection to the database was successful.</h3>';
+}
 
 phpinfo();
 ?>
